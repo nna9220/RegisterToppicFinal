2168071efd74d0b2d32fcb94622d9a2e4254a205
Nguyen <nna9220@gmail.com>
2023-12-12 21:26:12 +0700

    Phân GVPB

--- src/main/java/com/web/controller/HeadOfDepartment/AddCounterArgumentController.java
+++ src/main/java/com/web/controller/HeadOfDepartment/AddCounterArgumentController.java
@@ -1,5 +1,6 @@
 package com.web.controller.HeadOfDepartment;
 
+import com.web.config.CheckRole;
 import com.web.entity.Lecturer;
 import com.web.entity.Person;
 import com.web.entity.Subject;
@@ -7,6 +8,7 @@ import com.web.repository.LecturerRepository;
 import com.web.repository.PersonRepository;
 import com.web.repository.SubjectRepository;
 import com.web.service.Admin.SubjectService;
+import com.web.utils.UserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +16,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/headOfDepartment")
+@RequestMapping("/api/head/subject")
 public class AddCounterArgumentController {
     @Autowired
     private SubjectService subjectService;
@@ -29,6 +34,8 @@ public class AddCounterArgumentController {
     private SubjectRepository subjectRepository;
     @Autowired
     private LecturerRepository lecturerRepository;
+    @Autowired
+    private UserUtils userUtils;
     @PutMapping("/addCounterArgumrnt/{subjectId}")
     public ResponseEntity<?> addCounterArgumrnt(@PathVariable int subjectId, @RequestParam("LecturerId") String lecturerId){
         Subject existedSubject = subjectRepository.findById(subjectId).orElse(null);
@@ -46,6 +53,39 @@ public class AddCounterArgumentController {
             return ResponseEntity.ok(existedSubject);
         }else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping
+    public ModelAndView getDanhSachDeTai(HttpSession session){
+        Person personCurrent = CheckRole.getRoleCurrent(session, userUtils, personRepository);
+        if (personCurrent.getAuthorities().getName().equals("ROLE_HEAD")) {
+            Lecturer existedLecturer = lecturerRepository.findById(personCurrent.getPersonId()).orElse(null);
+            ModelAndView model = new ModelAndView("Duyet_TBM");
+            List<Subject> subjectByCurrentLecturer = subjectRepository.findSubjectByStatusAndMajor(false,existedLecturer.getMajor());
+            model.addObject("listSubject",subjectByCurrentLecturer);
+            return model;
+        }else {
+            ModelAndView error = new ModelAndView();
+            error.addObject("errorMessage", "Bạn không có quyền truy cập.");
+            return error;
+        }
+    }
+
+    @PostMapping("/browse/{id}")
+    public ModelAndView browseSubject(@PathVariable int id, HttpSession session, HttpServletRequest request){
+        Person personCurrent = CheckRole.getRoleCurrent(session, userUtils, personRepository);
+        if (personCurrent.getAuthorities().getName().equals("ROLE_HEAD") ) {
+            subjectService.browseSubject(id);
+            String referer = "http://localhost:5000/api/head/subject";
+            // Thực hiện redirect trở lại trang trước đó
+            System.out.println("Url: " + referer);
+            // Thực hiện redirect trở lại trang trước đó
+            return new ModelAndView("redirect:" + referer);
+        }else {
+            ModelAndView error = new ModelAndView();
+            error.addObject("errorMessage", "Bạn không có quyền truy cập.");
+            return error;
         }
     }
 }
