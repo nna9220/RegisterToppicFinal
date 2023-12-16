fc599df50e0324afbeb098fd92dd8694e258eef0
Nguyen <nna9220@gmail.com>
2023-12-17 00:06:49 +0700

    logout

--- src/main/java/com/web/controller/LogoutController.java
+++ src/main/java/com/web/controller/LogoutController.java
@@ -3,6 +3,8 @@ package com.web.controller;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
@@ -16,6 +18,10 @@ public class LogoutController {
     public String logout(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (auth != null) {
+            HttpSession session = request.getSession(false);
+            if (session!=null){
+                session.invalidate();
+            }
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
         return "redirect:/"; // Chuyển hướng đến trang chủ sau khi đăng xuất
