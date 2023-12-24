c035ff06a81f24896b3991ee7d855bb84c0d0b3a
nna9220 <102048288+nna9220@users.noreply.github.com>
2023-12-24 22:08:09 +0700

    Update LecturerRegisterTopicController.java

--- src/main/java/com/web/controller/Lecturer/LecturerRegisterTopicController.java
+++ src/main/java/com/web/controller/Lecturer/LecturerRegisterTopicController.java
@@ -79,7 +79,7 @@ public class LecturerRegisterTopicController {
     @GetMapping("/delete")
     public ModelAndView getDanhSachDeTaiDaXoa(HttpSession session){
         Person personCurrent = CheckRole.getRoleCurrent(session, userUtils, personRepository);
-        if (personCurrent.getAuthorities().getName().equals("ROLE_HEAD")) {
+        if (personCurrent.getAuthorities().getName().equals("ROLE_LECTURER")) {
             Lecturer existedLecturer = lecturerRepository.findById(personCurrent.getPersonId()).orElse(null);
             ModelAndView model = new ModelAndView("DeTaiBiXoa_GV");
             model.addObject("person", personCurrent);
